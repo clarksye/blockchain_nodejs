ad45338ba8f88666a82fd34912f5cadd239be6e3
clarksye <burhanbasyri@gmail.com>
2024-02-24 16:55:30 -0800

    POW : dynamic difficulty and the mine rate

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const  INITIAL_DIFFICULTY = 3;
+const MINE_RATE = 1000;
 
 const GENESIS_DATA = {
     timestamp: 1,
@@ -11,4 +12,5 @@ const GENESIS_DATA = {
 
 module.exports = {
     GENESIS_DATA,
+    MINE_RATE
 }
