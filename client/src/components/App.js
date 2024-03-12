5eb67d8ba08b0c38f4064c18c0a2fb8f6e1ac18b
clarksye <burhanbasyri@gmail.com>
2024-03-11 20:54:11 -0700

    add Blocks to the frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Blocks from './Blocks';
+
+class App extends Component {
+    state = { walletInfo: {} };
+
+    componentDidMount() {
+        fetch(`${document.location.origin}/api/wallet-info`)
+            .then(response => response.json())
+            .then(json => this.setState({ walletInfo: json }));
+    }
+
+    render() {
+        const { address, balance } = this.state.walletInfo;
+
+        return <div>
+            <div>Welcome to the blockchain...</div>
+            <div>Address: {address}</div>
+            <div>Balance: {balance}</div>
+            <br />
+            <Blocks />
+        </div>;
+    }
+}
+
+export default App; 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-  
-render(
-    <div>Cryptochain in React!: Third  Live Update</div>,
-    document.getElementById('root')
-);
+import App from './components/App';
+
+render(<App />, document.getElementById('root'));
